502dc1890a3a46beb85465183b3bebb966697515
wangy <wy799172467@163.com>
2016-08-16 10:49:20 +0800

    初始化

---
+++ GradleTest/testlabrary/src/main/java/com/example/testlabrary/MyActivity.java
@@ -0,0 +1,13 @@
+package com.example.testlabrary;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.geone.library.manager.MapViewManager;
+
+/**
+ * Created by wangy on 16/8/12.
+ */
+public class MyActivity extends AppCompatActivity {
+
+    MapViewManager mapViewManager = MapViewManager.getInstance();
+}
---
+++ MyLibrary/src/main/java/com/example/mylibrary/MyActivity.java
@@ -0,0 +1,12 @@
+package com.example.mylibrary;
+
+import com.geone.library.manager.MapViewManager;
+
+/**
+ * Created by wangy on 16/8/12.
+ */
+public class MyActivity  {
+
+
+
+}
