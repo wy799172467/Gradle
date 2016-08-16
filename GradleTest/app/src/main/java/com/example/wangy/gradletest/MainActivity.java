e30cd0ec6c7b706f573f2942e3232ca407126e56
wangy <wy799172467@163.com>
2016-08-16 13:51:59 +0800

    测试1

--- GradleTest/app/src/main/java/com/example/wangy/gradletest/MainActivity.java
+++ GradleTest/app/src/main/java/com/example/wangy/gradletest/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.wangy.gradletest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.geone.library.manager.MapViewManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        MapViewManager mapViewManager=MapViewManager.getInstance();
     }
 }
